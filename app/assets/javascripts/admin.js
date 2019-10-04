fd15cae5e5cb3017f141aeeab69050b90548d49e
tangling <tangling@pantrysbest.com>
2019-10-04 15:52:19 +0800

    admin

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,3 @@
+//= require jquery
+//= require third/bootstrap
+//= require third/bootstrap-dashboard
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-	root 'welcome#index'
+  root 'welcome#index'
   get 'welcome/index'
+  namespace :admin do
+    #root "wx_menus#index"
+    root "base#index"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
