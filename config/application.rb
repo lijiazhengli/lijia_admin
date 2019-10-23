9c587485b7bee813c5488443735258080b29cd3d
tangling <tangling@pantrysbest.com>
2019-10-23 09:16:52 +0800

    zh

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module LijiaAdmin
     config.time_zone = 'Beijing'
     config.active_record.default_timezone = :local
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :zh
+    config.i18n.locale = :zh
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
