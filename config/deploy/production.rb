29a0a9ec83965970c65e35b8492909ba9a1929af
tangling <tangling@pantrysbest.com>
2020-01-10 23:49:31 +0800

    branch name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :domain, '106.75.14.116'
 set :deploy_to, '/data/lijia_admin'
 set :repository, 'git@github.com:lijiazhengli/lijia_admin.git'
-set :branch, 'master'
+set :branch, 'production'
 set :user, 'root'
 set :rails_env, 'production'
