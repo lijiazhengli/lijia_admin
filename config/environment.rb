8f96288fce8b6c415aed60add5c3d2d951c149b0
tangling <tangling@pantrysbest.com>
2020-04-04 19:39:28 +0800

    放开不能下单的城市

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 QUANGUO_ALL_PROVINCE_NAMES = %w(北京 上海 天津 四川 广东 河北 江苏 安徽 浙江 重庆 贵州 云南 西藏 河南 湖北 湖南 广西 陕西 甘肃 青海 宁夏 新疆 山西 内蒙古 福建 江西 山东 辽宁 吉林 黑龙江 海南)
 
-QUANGUO_PRODUCT_INDELIVERY_PROVINCE_NAME = %w(新疆 青海 西藏 宁夏 内蒙古 甘肃)
+QUANGUO_PRODUCT_INDELIVERY_PROVINCE_NAME = [] # %w(新疆 青海 西藏 宁夏 内蒙古 甘肃)
 QUANGUO_PRODUCT_INDELIVERY_CITY_NAME = []
 QUANGUO_PRODUCT_INDELIVERY_AREA_NAME = []
 
