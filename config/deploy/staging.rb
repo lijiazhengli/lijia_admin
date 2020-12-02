5909a8b6a6e7ce3c9909b14f4a59aab500a1a2dd
lizhenjiang <lizhenjiang@soyoung.com>
2020-12-03 00:47:30 +0800

    branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :domain, '106.75.14.116'
 set :deploy_to, '/data/lijia_staging'
 set :repository, 'git@github.com:lijiazhengli/lijia_admin.git'
-set :branch, 'staging'
+set :branch, 'T2-staging'
 set :user, 'root'
 set :rails_env, 'staging'
