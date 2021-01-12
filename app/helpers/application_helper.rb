6e29868a423677b274e1202d9a9f6b8dc2033d81
tangling <tangling@pantrysbest.com>
2021-01-12 23:47:01 +0800

    经典案例

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def change_to_qiniu_https_url url
+    url ||= ''
     if url.include?(ENV['QINIU_URL']) and ENV['QINIU_URL_HTTPS'].present?
       url = url.gsub(ENV['QINIU_URL'], ENV['QINIU_URL_HTTPS'])
     elsif url.present? and !url.include?('http')
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def change_to_qiniu_https_url url
+    url ||= ''
     if url.include?(ENV['QINIU_URL']) and ENV['QINIU_URL_HTTPS'].present?
       url = url.gsub(ENV['QINIU_URL'], ENV['QINIU_URL_HTTPS'])
     end
